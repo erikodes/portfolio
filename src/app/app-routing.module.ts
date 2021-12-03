b5f396bca7362fadd1e002262cfd022414ce0081
Erik <erik@Eriks-MacBook-Pro.local>
2021-12-03 01:32:43 -0600

    primer intento de bug route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'splash',
-        pathMatch: 'full'
+        loadChildren: () => import('./pages/splash/splash.module').then(m => m.SplashPageModule)
     },
     {
         path: 'home',
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
-    },
-    {
-        path: 'splash',
-        loadChildren: () => import('./pages/splash/splash.module').then(m => m.SplashPageModule)
-    },
+    }
 ];
 
 @NgModule({
